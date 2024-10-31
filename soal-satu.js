4edbb7ed760f6332b39f63997829899fe3d626f3
Feridi <feridi@siloamhospitals.com>
2024-10-31 09:23:27 +0700

    update : new line

--- soal-satu.js
+++ soal-satu.js
@@ -13,9 +13,10 @@ Output :
 */
 
 // Diberikan sebuah array transactions yang berisi data transaksi dari beberapa user dengan format sebagai berikut:
-const transactions = [
+const transactions =
+[
   { transaction_id: 1, user_id: 101, items: [{ id: 'A1', qty: 2 }, { id: 'B1', qty: 3 }] },
-   { transaction_id: 2, user_id: 102, items:  [{ id: 'A1', qty: 1 }, { id: 'C1', qty: 5 }] },
+     { transaction_id: 2, user_id: 102, items:  [{ id: 'A1', qty: 1 }, { id: 'C1', qty: 5 }] },
    { transaction_id: 3, user_id: 101, items: [{ id: 'B1', qty: 4 }, { id: 'D1', qty: 1 }] },
   {  transaction_id: 4, user_id: 103, items: [{ id: 'A1', qty: 3 }, { id: 'B1', qty: 2 }] },
 ];
